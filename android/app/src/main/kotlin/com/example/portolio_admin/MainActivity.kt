d985df701ef9734a193654aa7bb7a8ed12aaf6d2
mudassir060 <mudassirmukhtar4@gmail.com>
2023-08-07 16:32:14 +0500

    ....

---
+++ android/app/src/main/kotlin/com/example/portolio_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portolio_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:portolio_admin/app/app.bottomsheets.dart';
+import 'package:portolio_admin/app/app.dialogs.dart';
+import 'package:portolio_admin/app/app.locator.dart';
+import 'package:portolio_admin/app/app.router.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [
+        StackedService.routeObserver,
+      ],
+    );
+  }
+}
