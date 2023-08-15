a66dde7c57e6321d8e8952f90c1aee1fee33708b
Laiba yousaf <laibaypusaf914@gmail.com>
2023-08-15 12:06:40 +0500

    pdf code

--- lib/ui/views/certificate/Widget/pdfwidget.dart
+++ lib/ui/views/certificate/Widget/pdfwidget.dart
@@ -1,6 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:http/http.dart' as http;
 
 class PDFScreen extends StatelessWidget {
   final String pdfAssetPath;
@@ -9,6 +12,26 @@ class PDFScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SfPdfViewer.network(pdfAssetPath);
+    return FutureBuilder(
+      future: _loadPdf(pdfAssetPath),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error loading PDF: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+
+         final pdfData = Uint8List.fromList(snapshot.data!);
+        return SfPdfViewer.memory(pdfData);
+        } else {
+          return const Text('No PDF data available.');
+        }
+      },
+    );
+  }
+
+  Future<List<int>> _loadPdf(String url) async {
+    final response = await http.get(Uri.parse(url));
+    return response.bodyBytes;
   }
 }
